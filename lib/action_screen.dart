7603c35afcd1078ae7d4814c7c92b01957d5db5e
KonstantinLeschenko <mypost.kh@gmail.com>
2021-11-01 13:41:15 +0200

    2

--- lib/action_screen.dart
+++ lib/action_screen.dart
@@ -1,16 +1,16 @@
+import 'package:awesome_button/awesome_button.dart';
 import 'package:erundople/questions.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ActionScreen extends StatefulWidget {
   const ActionScreen({Key? key}) : super(key: key);
-
 
   @override
   _ActionScreenState createState() => _ActionScreenState();
 }
 
 class _ActionScreenState extends State<ActionScreen> {
-
   int currentQuestion = 0;
   int correct = 0;
 
@@ -18,36 +18,43 @@ class _ActionScreenState extends State<ActionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('$correct / 30'),
+        title: Text('$currentQuestion / 30'),
       ),
       body: Column(
-        children: [
-          Text(questions[currentQuestion].word),
-          getButtons(questions[currentQuestion].answers)
-        ]
-
-      ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(questions[currentQuestion].word),
+            const SizedBox(
+              height: 60,
+            ),
+            getButtons(questions[currentQuestion].answers)
+          ]),
     );
-
   }
 
   getButtons(Map<String, bool> answers) {
     List<Widget> buttons = [];
     for (var answer in answers.keys) {
-      buttons.add(
-        TextButton(
-          onPressed: () {
-            if (answers[answer]!) {
-              setQuestion(true);
-              correct += 1;
-            } else {
-              setQuestion(false);
-            }
-            
-            },
-          child: Text(answer),
-        )
-      );
+      buttons.add(AwesomeButton(
+        borderRadius: BorderRadius.circular(25.0),
+        blurRadius: 10.0,
+        height: 40,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.blue,
+        child: Text(answer, style: const TextStyle(color: Colors.white)),
+        onTap: () {
+          if (answers[answer]!) {
+            setQuestion(true);
+            correct += 1;
+          } else {
+            setQuestion(false);
+          }
+        },
+      ));
+      buttons.add(const SizedBox(
+        height: 40,
+      ));
     }
 
     return ButtonBar(
@@ -60,10 +67,31 @@ class _ActionScreenState extends State<ActionScreen> {
       setState(() {
         currentQuestion += 1;
       });
-
     } else {
       print('Конец');
+
+      showDialog(
+          context: context,
+          builder: (_) => Dialog(
+                  child: SizedBox(
+                width: MediaQuery.of(context).size.width - 200,
+                height: MediaQuery.of(context).size.height - 200,
+                child: Center(
+                    child: Column(
+                  children: [
+                    Text('Результат $correct из 30'),
+                    const SizedBox(
+                      height: 150,
+                    ),
+                    TextButton(
+                        child: const Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const ActionScreen()));
+                        })
+                  ],
+                )),
+              )));
     }
   }
 }
-
