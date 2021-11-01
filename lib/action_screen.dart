ffeff9392be4d4eafc56f7180953bc06f9e6e5ac
KonstantinLeschenko <mypost.kh@gmail.com>
2021-11-01 23:29:37 +0200

    7

--- lib/action_screen.dart
+++ lib/action_screen.dart
@@ -20,44 +20,57 @@ class _ActionScreenState extends State<ActionScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
-        title: Text('${currentQuestion+1} / ${questions.length}'),
+        title: Text('${currentQuestion + 1} / ${questions.length}'),
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: (const BoxDecoration(
             gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [0.05, 0.5, 0.99],
-              colors: [Color(0xFF79b514), Color(0xFFe6fb8e), Color(0xFF79b514)],
-            )
-        )),
-
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.05, 0.5, 0.99],
+          colors: [Color(0xFF79b514), Color(0xFFe6fb8e), Color(0xFF79b514)],
+        ))),
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 100,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Text(
+                    questions[currentQuestion].word,
+                    style: TextStyle(fontSize: 50, color: Colors.green[900]),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'это',
+                    style: TextStyle(fontSize: 28, color: Colors.green[900]),
+                  ),
+                ],
               ),
+            ),
 
-              Text(
-                questions[currentQuestion].word,
-                style: TextStyle(fontSize: 60, color: Colors.green[900]),
+            Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 1,
+                  ),
+                  getButtons(questions[currentQuestion].answers)
+                ],
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              Text(
-                'это',
-                style: TextStyle(fontSize:30, color: Colors.green[900]),
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              getButtons(questions[currentQuestion].answers)
-            ]),
+            ),
+
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //children: [
+          ],
+        ),
       ),
     );
   }
@@ -68,52 +81,51 @@ class _ActionScreenState extends State<ActionScreen> {
       buttons.add(AwesomeButton(
         borderRadius: BorderRadius.circular(25.0),
         blurRadius: 10.0,
-        height: 60,
+        height: 50,
         width: MediaQuery.of(context).size.width,
         color: Colors.green,
-        child: Text(answer, style: const TextStyle(color: Colors.white, fontSize: 22), textAlign: TextAlign.center,),
+        child: Text(
+          answer,
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
         onTap: () {
           if (answers[answer]!) {
-            showDialog(context: context,
-                builder: (_)=> AlertDialog(
-                  title: const Text('Ерундопель'),
-                  content: const Text('Это правильный ответ'),
-                  actions: [
-                    TextButton(onPressed: () {
-                      Navigator.of(context, rootNavigator: true).pop();
-                      setQuestion(true);
-                      correct += 1;
-
-                    },
-                        child: const Text('OK'))
-                  ],
-
-                ));
-
-
+            showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                      title: const Text('Ерундопель'),
+                      content: const Text('Это правильный ответ'),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true).pop();
+                              setQuestion(true);
+                              correct += 1;
+                            },
+                            child: const Text('OK'))
+                      ],
+                    ));
           } else {
-            showDialog(context: context,
-                builder: (_)=> AlertDialog(
-                  title: const Text('Ерундопель'),
-                  content: const Text('Это НЕправильный ответ'),
-                  actions: [
-                    TextButton(onPressed: () {
-                      Navigator.of(context, rootNavigator: true).pop();
-                      setQuestion(false);
-                    },
-                        child: const Text('OK'))
-                  ],
-
-                ));
-
-
-
-
+            showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                      title: const Text('Ерундопель'),
+                      content: const Text('Это НЕправильный ответ'),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true).pop();
+                              setQuestion(false);
+                            },
+                            child: const Text('OK'))
+                      ],
+                    ));
           }
         },
       ));
       buttons.add(const SizedBox(
-        height: 40,
+        height: 20,
       ));
     }
 
